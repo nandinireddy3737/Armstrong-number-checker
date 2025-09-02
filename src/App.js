9e6ff45bcb39b3cc73ec93e6ff096f379e8882e8
nandiniredddy3737 <nandinireddy3737@gmail.com>
2025-09-02 19:06:40 +0530

    Added homepage field in package.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function ArmstrongChecker() {
+  const [number, setNumber] = useState('');
+  const [result, setResult] = useState('');
+
+  const isArmstrong = (num) => {
+    const digits = num.toString().split('');
+    const power = digits.length;
+    const sum = digits.reduce((acc, digit) => acc + Math.pow(Number(digit), power), 0);
+    return sum === Number(num);
+  };
+
+  const handleCheck = () => {
+    if (number === '') {
+      setResult('Please enter a number.');
+      return;
+    }
+
+    const armstrong = isArmstrong(number);
+    setResult(
+      armstrong
+        ? `${number} is an Armstrong number`
+        : `${number} is not an Armstrong number`
+    );
+  };
+
+  return (
+    <div>
+      <h2>Armstrong Number Checker</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        placeholder="Enter a number"
+      />
+      <button onClick={handleCheck}>
+        Check
+      </button>
+      <p>{result}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArmstrongChecker />
     </div>
   );
 }
